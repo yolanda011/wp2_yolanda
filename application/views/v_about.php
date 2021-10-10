a7caef33089221b990107aee3a32ed04d32bce93
yolanda011 <gecko18521@gmail.com>
2021-10-10 21:05:44 +0700

    web programing2

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan    : yolanda</li>
+ <li>Nama Belakang : sinaga</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li>matraman</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>korea</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>baca wp</li>
+ <li>rebahan</li>
+ </ul>
+</section>
--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,15 +1,15 @@
 <section>
-
-       <h1><?php echo $judul ?></h1>
-       <p align='justify'>Pada pengertian codeigniter di atas 
-tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih 
+ <h1><?php echo $judul ?></h1>
+ <p align='justify'>Pada pengertian codeigniter di atas 
+tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu
+MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih 
 jauh dalam belajar codeigniter.</p>
-          <p>>MVC adalah teknik atau konsep yang memisahkan 
+ <p>MVC adalah teknik atau konsep yang memisahkan 
 komponen utama menjadi tiga komponen yaitu model, view dan 
 controller.</p>
-
-        <ol type="a">
-        <li>model</li>
+ 
+ <ol type="a">
+ <li>Model</li>
 <p align='justify'>Model adalah kelas yang merepresentasikan atau 
 memodelkan tipe data yang akan digunakan oleh aplikasi. Model juga 
 dapat didefinisakn sebagai bagian penanganan yang berhubungan dengan 
@@ -21,15 +21,14 @@ untuk menghitung luas dan keliling lingkaran, maka dapat memodelkan
 objek lingkaran sebagai kelas model.</p>
 <p align='justify'>Sebagai catatan, Semua model harus disimpan di 
 dalam folder application\models</p>
-<li>viuw</li>
-<p align+'justify'>View merupakan bagian yang menangani halaman user 
+<li>View</li>
+<p align='justify'>View merupakan bagian yang menangani halaman user 
 interface atau halaman yang muncul pada user(pada browser). Tampilan 
 dari user interface di kumpulkan pada view untuk memisahkannya 
 dengan controller dan model sehingga memudahkan web designer dalam 
 melakukan pengembangan tampilan halaman website.</p>
-<li>controller</li>
-<p align='justify'>>
-<p align=’justify’>Controller merupakan kumpulan intruksi aksi yang 
+<li>Controller</li>
+<p align='justify'>Controller merupakan kumpulan intruksi aksi yang 
 menghubungkan model dan view, jadi user tidak akan berhubungan 
 dengan model secara langsung, intinya data yang tersimpan di
 database (model) di ambil oleh controller dan kemudian controller 
@@ -44,7 +43,8 @@ meneruskannya pada view untuk di tampilkan. Jadi jelas sudah dan
 sangat mudah dalam pengembangan aplikasi dengan cara mvc ini karena 
 web designer atau front-end developer tidak perlu lagi berhubungan 
 dengan controller, dia hanya perlu berhubungan dengan view untuk 
-mendesign tampilann aplikasi, karena back-end developer yang menangani bagian controller dan modelnya. Jadi pembagian tugas pun 
+mendesign tampilann aplikasi, karena back-end developer yang 
+menangani bagian controller dan modelnya. Jadi pembagian tugas pun 
 menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan
 cepat dan terstruktur.</p>
-       </section>
+ </section>
